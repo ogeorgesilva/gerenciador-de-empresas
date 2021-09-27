6b99e0e1c0ea593fcf010d8df89872e7f2fbc4d5
George Silva <George-silva@live.com>
2021-09-27 11:09:04 -0300

    Projeto de um gerenciador de empresas

---
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,60 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings({ "deprecation"})
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Alura");
+		empresa.setDataAbertura(new Date("2013/06/01"));
+		
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
+		empresa2.setNome("Caelum");
+		empresa2.setDataAbertura(new Date("2003/02/18"));
+		
+		
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+
+	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
+		lista.add(empresa);
+	}
+	
+	public List<Empresa> getEmpresas() {
+		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+}
