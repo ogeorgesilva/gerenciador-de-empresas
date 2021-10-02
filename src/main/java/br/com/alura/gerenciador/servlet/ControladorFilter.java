b89cad3110c631bc27bb5e63e4bd878546bd6b89
George Silva <George-silva@live.com>
2021-10-02 16:19:06 -0300

    Implementando Filtros/Interceptadores

--- src/main/java/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -2,34 +2,28 @@ package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
-//@WebServlet("/entrada")
-public class UnicaEntradaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class ControladorFilter implements Filter {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
+		System.out.println("ControladorFilter");
+		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		
 		String paramAcao = request.getParameter("acao");
-		
-//		HttpSession sessao = request.getSession();
-//		boolean usuarioNaoLogado = sessao.getAttribute("usuarioLogado") == null;
-//		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
-//		
-//		if(ehUmaAcaoProtegida && usuarioNaoLogado) {
-//			response.sendRedirect("entrada?acao=LoginForm");
-//			return;
-//		}
-		
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 		String nome;
 		
@@ -54,7 +48,7 @@ public class UnicaEntradaServlet extends HttpServlet {
 			response.sendRedirect(tipoEEndereço[1]);
 
 		}
-
+		
 	}
 
 }
